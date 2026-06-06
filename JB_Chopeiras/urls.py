8edd494819fe9672fcde5f25ebbbd75f02715442
Samuel Laureano Silva <samuellaureanosilva7@gmail.com>
2026-06-06 19:14:36 -0300

    Define home como página inicial

--- JB_Chopeiras/urls.py
+++ JB_Chopeiras/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/home/', permanent=False)),
     path('home/', views.home, name='home'),
     path('entrada/', views.tela_entrada, name='tela_entrada'),
     path('sair/', views.sair, name='sair'),
